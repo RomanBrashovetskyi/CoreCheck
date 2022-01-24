4a5a84f740d73ba8e497b421d54edffa07b3e300
RomanBrashovetskyi <eagle.w7@gmail.com>
2022-01-24 23:33:01 +0200

    comments

--- src/t2/ClassInitDemo1.java
+++ src/t2/ClassInitDemo1.java
@@ -4,21 +4,19 @@ public class ClassInitDemo1 {
 
         public ClassInitDemo1() {
             System.out.println("constructor");
-        }
+        }//4
 
         { // non-static
-            System.out.println("non-static block");
+            System.out.println("non-static block");//3
         }
 
         static { // static
-            System.out.println("static block");
+            System.out.println("static block");//1
         }
 
         public static void main(String[] arg) {
-            System.out.println("main");
+            System.out.println("main");//2
             ClassInitDemo1 cdm = new ClassInitDemo1();
         }
-
-
     }
 
