9a9f98d6763f24a62441d97d165c6885d735024b
RomanBrashovetskyi <eagle.w7@gmail.com>
2022-01-30 23:18:58 +0200

    comments

--- src/t3/MethodsOfOjlect.java
+++ src/t3/MethodsOfOjlect.java
@@ -3,12 +3,13 @@ package t3;
 public class MethodsOfOjlect {
     Car car1 = new Car(1);
 
-    public MethodsOfOjlect(){
+    public MethodsOfOjlect() {
         System.out.println(car1.getClass());
-
+        System.out.println(car1.hashCode());
+        System.out.println(car1.toString());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new MethodsOfOjlect();
     }
 }
